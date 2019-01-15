b6d62ebbe53480c41e6fee6c4b6af8262e3c80cc
Mr. Mahesh Sunkara <46166792+MAHESH9598@users.noreply.github.com>
2019-01-15 19:42:29 +0530

    Initial commit

---
+++ str.py
@@ -0,0 +1,33 @@
+#this is Example for ------------>str
+
+""" Definition : --> str is one of the class and treated as fundamental datatype.
+
+                 --> The purpose of this data type is to store string data [collection(or)sequence of charectors].
+
+                 --> One can store String Data in 4 ways.
+
+                                     --> With in double quotes (" ")
+
+                                     --> With in single quotes (' ')
+
+                                     --> Tripple Single quotes ('''  ''')
+
+                                     --> Tripple double quotes ("""  """)
+"""
+
+#### EXAMPLES  :
+
+A="Python/Django Developer  Mahesh Bhagavath"
+B='Python/Django Developer  Mahesh Bhagavath'
+C="""Python/Django Developer  Mahesh Bhagavath"""
+D='''Python/Django Developer  Mahesh Bhagavath'''
+
+#====> 'mahi',"mahi" this two are called as Single Quoted Strings
+#====> """mahi""", '''mahi''' this two are called as Double Quoted Strings
+
+print("A ---->    ",A,type(A))
+print("B ---->    ",B,type(B))
+print("C ---->    ",C,type(C))
+print("D ---->    ",D,type(D))
+
+
